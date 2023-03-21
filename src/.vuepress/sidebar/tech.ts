52d9c95588905414b9f3837637afd00989e96294
dhb <xx158@qq.com>
2023-03-21 21:21:53 +0800

    修改侧边栏顺序

--- src/.vuepress/sidebar/tech.ts
+++ src/.vuepress/sidebar/tech.ts
@@ -1,7 +1,32 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export const techSidebar = sidebar({
-  "/tech": "structure",
-  "/": [
+  "/tech/": [
+    "",
+    {
+      text: "Java",
+      prefix: "java",
+      collapsible: true,
+      children: "structure"
+    },
+    {
+      text: "Linux",
+      prefix: "linux",
+      collapsible: true,
+      children: "structure"
+    },
+    {
+      text: "网络",
+      prefix: "网络",
+      collapsible: true,
+      children: "structure"
+    },
+    {
+      text: "其他",
+      prefix: "其他",
+      collapsible: true,
+      children: "structure"
+    }
   ],
+  "/": [],
 });
