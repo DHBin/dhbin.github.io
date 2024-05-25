6243560f45a479a496bd6756bdeee40f66a66986
dhb <xx158@qq.com>
2024-05-26 01:59:15 +0800

    refactor: 移除md增强

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -81,46 +81,5 @@ export default hopeTheme({
       apiKey: 'f9a056d506baba509dc762fd42407c3a',
       indexName: 'dhbin',
     },
-
-    // all features are enabled for demo, only preserve features you need here
-    mdEnhance: {
-      align: true,
-      attrs: true,
-      chart: true,
-      codetabs: true,
-      demo: true,
-      echarts: true,
-      figure: true,
-      flowchart: true,
-      gfm: true,
-      imgLazyload: true,
-      imgSize: true,
-      include: true,
-      katex: true,
-      mark: true,
-      mermaid: true,
-      playground: {
-        presets: ["ts", "vue"],
-      },
-      revealJs: { plugins: ["highlight", "math", "search", "notes", "zoom"] },
-      stylize: [
-        {
-          matcher: "Recommended",
-          replacer: ({ tag }) => {
-            if (tag === "em")
-              return {
-                tag: "Badge",
-                attrs: { type: "tip" },
-                content: "Recommended",
-              };
-          },
-        },
-      ],
-      sub: true,
-      sup: true,
-      tabs: true,
-      vPre: true,
-      vuePlayground: true,
-    },
   },
 });
